c7660d4031c19bf51aa18b8ba7220019459bf133
xpadev <xpadev@gmail.com>
2024-02-18 18:35:07 +0900

    feat: build.sh

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -z "$GIT_REPO" ]; then
+  echo "GIT_REPOが設定されていません。"
+  exit 1
+fi
+
+if [ -z "$IMAGE_NAME" ]; then
+  echo "IMAGE_NAMEが設定されていません。"
+  exit 1
+fi
+
+if [ -z "$IMAGE_TAG" ]; then
+  echo "IMAGE_TAGが設定されていません。"
+  exit 1
+fi
+
+if [ -z "$DOCKER_REGISTRY" ]; then
+  echo "DOCKER_REGISTRYが設定されていません。"
+  exit 1
+fi
+
+if [ -z "$DOCKER_REGISTRY_PASSWORD" ]; then
+  echo "DOCKER_REGISTRY_PASSWORDが設定されていません。"
+  exit 1
+fi
+
+git clone $GIT_REPO source_code
+cd source_code
+
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
+
+echo $DOCKER_REGISTRY_PASSWORD | docker login $DOCKER_REGISTRY --username $DOCKER_REGISTRY_USER --password-stdin
+
+docker push $IMAGE_NAME:$IMAGE_TAG
